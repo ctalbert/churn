034d6933d4338e9df0a0f0066a32fe88894925d3
Clint Talbert <ctalbert@mozilla.com>
2014-11-04 15:42:12 -0800

    pushing up the helper script for gathering data using the metrics command

---
+++ go-metrics.sh
@@ -0,0 +1,39 @@
+cd ~/projects/m-a
+# Nightly
+cd ~/projects/m-c
+hg metrics -f ~/projects/churn/nightly-35.json -d "2014-07-22 to 2014-09-02"
+hg metrics -f ~/projects/churn/nightly-34.json -d "2014-06-10 to 2014-07-21"
+hg metrics -f ~/projects/churn/nightly-33.json -d "2014-04-29 to 2014-06-09"
+hg metrics -f ~/projects/churn/nightly-32.json -d "2014-03-18 to 2014-04-28"
+hg metrics -f ~/projects/churn/nightly-31.json -d "2014-02-04 to 2014-03-17"
+hg metrics -f ~/projects/churn/nightly-30.json -d "2013-12-10 to 2014-02-03"
+hg metrics -f ~/projects/churn/nightly-29.json -d "2013-10-29 to 2013-12-09"
+hg metrics -f ~/projects/churn/nightly-28.json -d "2013-09-17 to 2013-10-28"
+hg metrics -f ~/projects/churn/nightly-27.json -d "2013-08-06 to 2013-09-16"
+hg metrics -f ~/projects/churn/nightly-26.json -d "2013-06-25 to 2013-08-05"
+hg metrics -f ~/projects/churn/nightly-25.json -d "2013-05-14 to 2013-06-24"
+
+# Aurora
+cd ~/projects/m-a
+hg metrics -f ~/projects/churn/aurora-34.json -d "2014-07-22 to 2014-09-02"
+hg metrics -f ~/projects/churn/aurora-33.json -d "2014-06-10 to 2014-07-21"
+hg metrics -f ~/projects/churn/aurora-32.json -d "2014-04-29 to 2014-06-09"
+hg metrics -f ~/projects/churn/aurora-31.json -d "2014-03-18 to 2014-04-28"
+hg metrics -f ~/projects/churn/aurora-30.json -d "2014-02-04 to 2014-03-17"
+hg metrics -f ~/projects/churn/aurora-29.json -d "2013-12-10 to 2014-02-03"
+hg metrics -f ~/projects/churn/aurora-28.json -d "2013-10-29 to 2013-12-09"
+hg metrics -f ~/projects/churn/aurora-27.json -d "2013-09-17 to 2013-10-28"
+hg metrics -f ~/projects/churn/aurora-26.json -d "2013-08-06 to 2013-09-16"
+hg metrics -f ~/projects/churn/aurora-25.json -d "2013-06-25 to 2013-08-05"
+
+#Beta
+cd ~/projects/m-b
+hg metrics -f ~/projects/churn/beta-33.json -d "2014-07-22 to 2014-09-02"
+hg metrics -f ~/projects/churn/beta-32.json -d "2014-06-10 to 2014-07-21"
+hg metrics -f ~/projects/churn/beta-31.json -d "2014-04-29 to 2014-06-09"
+hg metrics -f ~/projects/churn/beta-30.json -d "2014-03-18 to 2014-04-28"
+hg metrics -f ~/projects/churn/beta-29.json -d "2014-02-04 to 2014-03-17"
+hg metrics -f ~/projects/churn/beta-28.json -d "2013-12-10 to 2014-02-03"
+hg metrics -f ~/projects/churn/beta-27.json -d "2013-10-29 to 2013-12-09"
+hg metrics -f ~/projects/churn/beta-26.json -d "2013-09-17 to 2013-10-28"
+hg metrics -f ~/projects/churn/beta-25.json -d "2013-08-06 to 2013-09-16"
